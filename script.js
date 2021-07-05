d3ea65c324eb56d7688a77c7835618f30940acf1
Paulo3678 <ph7811001@gmail.com>
2021-07-05 09:41:04 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,62 @@
+function rodar1(){
+    let icone = document.getElementById('icone1');
+    if(true){
+        document.getElementById('borda1').style.transform = 'rotate(30deg)';
+    }
+}
+
+function voltar1(){
+    let icone = document.getElementById('icone1');
+    if(true){
+        document.getElementById('borda1').style.transform = '';
+    }
+}
+
+
+
+function rodar2(){
+    let icone = document.getElementById('icone2');
+    if(true){
+        document.getElementById('borda2').style.transform = 'rotate(30deg)';
+    }
+}
+
+function voltar2(){
+    let icone = document.getElementById('icone2');
+    if(true){
+        document.getElementById('borda2').style.transform = '';
+    }
+}
+
+
+
+
+function rodar3(){
+    let icone = document.getElementById('icone3');
+    if(true){
+        document.getElementById('borda3').style.transform = 'rotate(30deg)';
+    }
+}
+
+function voltar3(){
+    let icone = document.getElementById('icone3');
+    if(true){
+        document.getElementById('borda3').style.transform = '';
+    }
+}
+
+
+
+function rodar4(){
+    let icone = document.getElementById('icone4');
+    if(true){
+        document.getElementById('borda4').style.transform = 'rotate(30deg)';
+    }
+}
+
+function voltar4(){
+    let icone = document.getElementById('icone4');
+    if(true){
+        document.getElementById('borda4').style.transform = '';
+    }
+}
